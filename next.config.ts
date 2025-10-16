78cb3b3c3e680b5a4040c4dd4908b555f7c4d469
arnnvv <arnav.900@protonmail.com>
2025-10-16 07:22:14 +0530

    compiler

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: {
+      panicThreshold: "ALL_ERRORS",
+    },
+  },
 };
 
 export default nextConfig;
